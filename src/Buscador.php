1902501601cc7b1603b3b352163098815265deee
lucasAlcp <lucas.al.cp@gmail.com>
2022-08-10 22:06:54 -0300

    Adicionado projeto do Curso da Alura de Composer

---
+++ src/Buscador.php
@@ -0,0 +1,41 @@
+<?php
+namespace Alura\BuscadorDeCursos;
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Psr\Http\Client\ClientInterface;
+
+
+class Buscador{
+    private $httpClient;
+    private $crawler;
+
+
+    public function __construct( $httpClient,  $crawler){
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url){
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+
+        $cursos = [];
+
+        foreach($elementosCursos as $elemento){
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+
+}
+
+?>
